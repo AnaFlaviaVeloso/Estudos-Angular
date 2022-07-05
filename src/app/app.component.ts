14aff58231928ad24a37a9422dc2e9999d7fa292
AnaFlaviaVeloso <anaflaviaveloso90@gmail.com>
2022-07-05 17:09:04 -0300

    Implementando Diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project3';
+export class AppComponent implements OnInit{
+    count = 0;
+    nome = 'Ana Flávia Veloso';
+    text = '';
+
+    pessoas = [
+      {
+        nome: "Ana",
+        sobrenome: "Veloso"
+      },
+      {
+        nome: "Matias",
+        sobrenome: "Silva"
+      },
+      {
+        nome: "Aline",
+        sobrenome: "Costa"
+      },
+      {
+        nome: "Jack",
+        sobrenome: "Cunha"
+      }
+    ]
+
+    constructor(){
+
+    }
+
+    ngOnInit(){
+      console.log(this.pessoas);
+      let interval = setInterval(() => {
+        this.count++;
+        if(this.count === 10) {
+          clearInterval(interval)
+        }
+      }, 1000)
+    }
+
+    clicou(nome: string): void {
+      console.log('Clicou em mim', nome)
+    }
 }
